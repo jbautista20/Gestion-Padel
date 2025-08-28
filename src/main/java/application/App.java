23249bf31b2dd06f2bf704c66e44bace68504dd5
Bauti <juanbmontenegro20@gmail.com>
2025-08-27 23:07:16 -0300

    Agregando print

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -24,6 +24,7 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
         System.out.println("EL PATA");
+        System.out.println("ESTE PRINT DEBERIA GENERAR CONFLICTO");
 
     }
 
