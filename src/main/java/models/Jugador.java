fed66332daa90805dddf83b2a05ed4e3be455006
Bauti <juanbmontenegro20@gmail.com>
2025-09-28 21:12:46 -0300

    basededatos+pantallas

--- src/main/java/models/Jugador.java
+++ src/main/java/models/Jugador.java
@@ -5,14 +5,14 @@ public class Jugador extends Persona {
     private int Categoria;
     private int Sexo;
     private int Puntos;
-    private LocalDate Año_Nac;
+    private LocalDate Anio_Nac;
 
-    public Jugador(String Nombre, String Apellido, String Telefono, String Direccion, int Categoria, int Sexo, int Puntos, LocalDate Año_Nac) {
+    public Jugador(String Nombre, String Apellido, String Telefono, String Direccion, int Categoria, int Sexo, int Puntos, LocalDate Anio_Nac) {
         super(Nombre, Apellido, Telefono, Direccion);
         this.Categoria = Categoria;
         this.Sexo = Sexo;
         this.Puntos = Puntos;
-        this.Año_Nac = Año_Nac;
+        this.Anio_Nac = Anio_Nac;
     }
 
     public int getCategoria() { return Categoria; }
@@ -24,8 +24,8 @@ public class Jugador extends Persona {
     public int getPuntos() { return Puntos; }
     public void setPuntos(int Puntos) { this.Puntos = Puntos; }
 
-    public LocalDate getAño_Nac() { return Año_Nac; }
-    public void setAño_Nac(LocalDate Año_Nac) { this.Año_Nac = Año_Nac; }
+    public LocalDate getAnio_Nac() { return Anio_Nac; }
+    public void setAnio_Nac(LocalDate Anio_Nac) { this.Anio_Nac = Anio_Nac; }
 
 
 }
--- src/main/java/utilities/Paths.java
+++ src/main/java/utilities/Paths.java
@@ -3,4 +3,8 @@ package utilities;
 public class Paths {
 
     public static final String pantallaUno = "/inicio.fxml";
+    public static final String pantallaJugadores = "/viewJugadores.fxml";
+    public static final String pantallaTorneos = "/viewTorneos.fxml";
+    public static final String pantallaTurnos = "/viewTurnos.fxml";
+
 }
